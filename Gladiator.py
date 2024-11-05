b10b05d87dba977d98a24abe89874d6d49161bcd
Isac Hull <isac.hull@learnet.se>
2024-11-05 10:38:13 +0100

    Version 1 av spelet

---
+++ Gladiator.py
@@ -0,0 +1,23 @@
+### GRUNDSPELET ###
+
+# Steg 1: Skriv en introtext med printsatser som berättar var spelaren är och vad den ska göra
+
+# Steg 2: Gör så att spelaren kan attackera med slag och spark (använd t.ex. hälsopoäng och simulera tärninslag om du vill)
+
+# Steg 3: Gör så motståndaren kan anfalla spelaren
+
+# Steg 4: Berätta vem som vann striden
+
+### GER ett E ###
+
+### FÖR HÖGRE BETYG ###
+
+# Gör så många funktioner du kan från listan i uppgiftsbeskrivningen.
+
+def intro():
+    print("Välkommen till Gladiatorernas Arena!")
+    print("Du är en modig gladiator, redo att möta dina motståndare.")
+    print("Din uppgift är att besegra din fiende genom att sänka deras hälspoäng till noll.")
+    print("Lycka till!")
+
+intro()
